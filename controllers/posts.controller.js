0d72c3137bf0d6385a14cc039b20e7f957db8e5b
parthsingh <parthsingh1910>
2023-08-21 11:35:55 +0530

    Added comment and post controller and client and server file

---
+++ controllers/posts.controller.js
@@ -0,0 +1,29 @@
+let posts = [{
+    user: 'Two-Face',
+    title: 'How to Flip a Coin'
+}, {
+    user: 'Joker',
+    title: 'Top 5 Jokes of 2015'
+}];
+module.exports = function (socket) {
+    // Recent Posts
+    for (let i = 0; i<posts.length; i++) {
+        socket.emit('post.add', posts[i]);
+        socket.emit('posts.count', {
+            count: i + 1
+        });
+    }
+};
+
+let i = 0;
+const addingPosts = setInterval(function () {
+    if (posts[i]) {
+        socket.emit('post.add', posts[i]);
+        socket.emit('posts.count', {
+            count: i + 1
+        });
+        i++;
+    } else {
+        clearInterval(addingPosts);
+    }
+}, 2000);
--- server.js
+++ server.js
@@ -1,21 +1,28 @@
-var express = require('express'),
+let express = require('express'),
     app = express(),
     http = require('http'),
     socketIO = require('socket.io'),
     server, io;
 
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/client.html');
-});
+    res.sendFile(__dirname + 'index.html')
+})
 
-server = http.Server(app);
-server.listen(5000);
+server = http.Server(app)
+
+server.listen(5000)
+
+console.log("Hello i am a server JS")
+
 io = socketIO(server);
+
 io.on('connection', function (socket) {
-    socket.emit('greeting-from-server', {
-        greeting: 'Hello Client'
-    });
-    socket.on('greeting-from-client', function (message) {
-        console.log(message);
-    });
+    console.log(`"Hii`)
+    let controllers = ['comments', 'posts']
+
+    for (let i = 0; i<controllers.length; i++) {
+        require('./controllers/' + controllers[i] +
+            '.controller')(socket);
+    }
+
 })
